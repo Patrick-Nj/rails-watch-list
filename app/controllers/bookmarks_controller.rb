cdc8b474b55e5f765a7d9356838c6a3aac3188b9
patrick njoume <p.njoume@gmail.com>
2022-05-20 12:27:20 +0200

    Ex2suite

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,3 +1,31 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
 
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 end
